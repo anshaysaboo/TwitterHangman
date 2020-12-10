f940abdaa89e9b52b587d6b60664e3fe1d6b367c
anshaysaboo <anshaysaboo1586@gmail.com>
2020-12-10 15:07:14 -0800

    Fixed bug where front end is not visible in production

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const KEYS = require("./config/keys.js");
 
 const app = express();
 
+// GET /api/trending-topics
+// Queries the Twitter API for a list of trending topics and sends it back to the client
 app.get("/api/trending-topics", async (req, res) => {
   try {
     const result = await axios.get(
@@ -24,6 +26,8 @@ app.get("/api/trending-topics", async (req, res) => {
   }
 });
 
+// GET /api/search-tweets/:query
+// Uses the Twitter API to search for tweets with the given query
 app.get("/api/search-tweets/:query", async (req, res) => {
   try {
     const result = await axios.get(
@@ -48,6 +52,18 @@ app.get("/api/search-tweets/:query", async (req, res) => {
     res.status(500).send({ message: err.message });
   }
 });
+
+// Display the front-end web application in a production environment
+if (process.env.NODE_ENV !== "development") {
+  // Serve main.js, main.css, and other specific assets
+  app.use(express.static("client/build"));
+
+  // if route not recognized, show HTML
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
